74cf658a82003678e867e4292077ccd6bf333c34
tecknickle <70725839+tecknickle@users.noreply.github.com>
2025-03-26 17:49:58 -0700

    Initial commit.

---
+++ Assets/Nick's Tools/Setup.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEngine;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static UnityEditor.AssetDatabase;
+
+public class Setup
+{
+    [MenuItem("Edit/Setup/Create Default Folders")]
+    public static void CreateDefaultFolders()
+    {
+        Folders.CreateDefault("_Project", "Animation", "Art", "Materials", "Prefabs", "ScriptableObjects", "Scripts", "Sounds", "Settings");
+        Refresh();
+    }
+
+}
+
+static class Folders
+{
+    public static void CreateDefault(string root, params string[] folders)
+    {
+        var fullpath = Combine(Application.dataPath, root);
+        foreach (var folder in folders)
+        {
+            var path = Combine(fullpath, folder);
+            if (!Exists(path))
+            {
+                CreateDirectory(path);
+            }
+        }
+
+    }
+}
